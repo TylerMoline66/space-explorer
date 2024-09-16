e2f8186b074bf1c3e658a94f37bc757741287f03
Tyler Moline <tylermoline95@gmail.com>
2024-09-16 16:24:55 -0600

    add planets and labels

--- src/Components/Planet1.jsx
+++ src/Components/Planet1.jsx
@@ -1,8 +1,8 @@
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
-import { Sphere } from "@react-three/drei";
+import { Sphere, Text } from "@react-three/drei";
 
-function Planet({ position, color, onPlanetClick }) {
+function Planet({ position, color, onPlanetClick, label }) {
   const planetRef = useRef();
 
   // Make the planet rotate continuously
@@ -18,6 +18,16 @@ function Planet({ position, color, onPlanetClick }) {
     >
       <sphereGeometry args={[1, 32, 32]} />
       <meshStandardMaterial color={color} />
+
+      <Text
+        position={[0, 1.5, 0]} // Position above the planet
+        fontSize={0.4} // Adjust font size
+        color="white" // Text color
+        anchorX="center" // Center text horizontally
+        anchorY="middle" // Align text vertically
+      >
+        {label} {/* Display the label passed as a prop */}
+      </Text>
     </mesh>
   );
 }
--- src/Components/Space.jsx
+++ src/Components/Space.jsx
@@ -32,6 +32,7 @@ function SpaceScene() {
         position={[0, 0, 0]}
         color="grey"
         onPlanetClick={() => handlePlanetClick([0, 0, 0])}
+        label="About Me!"
       />
 
       {/* Planet on the Left */}
@@ -39,6 +40,7 @@ function SpaceScene() {
         position={[-5, 0, -5]}
         color="salmon"
         onPlanetClick={() => handlePlanetClick([-5, 0, -5])}
+        label="Utah Basement Builders"
       />
 
       {/* Planet on the Right */}
@@ -46,6 +48,35 @@ function SpaceScene() {
         position={[5, 0, -5]}
         color="pink"
         onPlanetClick={() => handlePlanetClick([5, 0, -5])}
+        label="Tsquared Construction"
+      />
+
+      <Planet
+        position={[3, 4, -5]}
+        color="red"
+        onPlanetClick={() => handlePlanetClick([3, 4, -5])}
+        label="Resonance"
+      />
+
+      <Planet
+        position={[-3, 4, -5]}
+        color="blue"
+        onPlanetClick={() => handlePlanetClick([-3, 4, -5])}
+        label="Taskwize"
+      />
+
+      <Planet
+        position={[-3, -4, -5]}
+        color="coral"
+        onPlanetClick={() => handlePlanetClick([-3, -4, -5])}
+        label="Earth"
+      />
+
+      <Planet
+        position={[3, -4, -5]}
+        color="purple"
+        onPlanetClick={() => handlePlanetClick([3, -4, -5])}
+        label="Earth"
       />
     </Canvas>
   );
